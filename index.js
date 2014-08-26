c67fd77a3e69e761a519f2994b24322c05e29b0a
Karl Böhlmark <karl.bohlmark@gmail.com>
2014-08-26 17:07:01 +0200

    better

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ var ObservableCollection = require("observable-collection");
 module.exports = FilteredCollection;
 
 function FilteredCollection (items, filter) {
-    var filteredItems = items.filter(filter);
-    ObservableCollection.call(this, filteredItems);
+    this._filter = filter;
+    this._unfiltered = items;
+    this._reset = ObservableCollection.prototype.reset;
+    ObservableCollection.call(this, this._filtered());
     var self = this;
     filter.on('change', function () {
-        self.reset(items.filter(filter));
+        self._reset(self._filtered());
     })
 }
 
 FilteredCollection.prototype = Object.create(ObservableCollection.prototype);
+
+FilteredCollection.prototype._filtered = function () {
+    return this._unfiltered.filter(this._filter);
+}
+
+FilteredCollection.prototype.reset = function (unfiltered) {
+    this._unfiltered = unfiltered;
+    this._reset(this._filtered());
+}
